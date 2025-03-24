2a11f72327878e6611a6bc9f000f00e2a1934bd4
Merlottera <sasaquerie@gmail.com>
2025-03-23 22:20:11 -0300

    Código Melhorado e atualizado em inglês

--- src/game.py
+++ src/game.py
@@ -1,94 +1,102 @@
 import pygame
 
-LARGURA, ALTURA = 800, 640
-TITULO = "Minha Janela com Pygame-CE"
 
-def __main__():
-# Inicializa o pygame
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 640
+WINDOW_TITLE = 'News Please'
+
+
+def display_data(data, news, window):
+    width = window.get_width()
+    font = pygame.font.Font(None, 12)
+    line_y_offset = 120
+    news_line_spacing = 15
+
+    sections = []
+    for chave, valor in data.items():
+        text = f'{chave}: {valor}'
+        words = text.split()
+        current_line = ''
+        pos_x = (width / 2) - news.get_width() / 2 + 10
+        max_text_width = news.get_width() - 20  # Margem dentro da folha
+        pos_y = line_y_offset  # Guarda a posição inicial da caixa
+        section_width = 0  # Largura máxima da caixa
+        section_height = 0  # Altura acumulada da caixa
+
+        for word in words:
+            if font.size(current_line + word + " ")[0] > max_text_width:
+                rendered = font.render(current_line, True, (0, 0, 0))
+                window.blit(rendered, (pos_x, line_y_offset))
+                section_width = max(section_width, rendered.get_width())
+                section_height += rendered.get_height()
+                line_y_offset += news_line_spacing
+                section_height += news_line_spacing
+                current_line = f'{word} '
+                break
+                
+            if chave == 'date':
+                pos_x = width/2 + 100
+                
+            current_line += f'{word} '
+
+        if current_line:  # Renderiza a última linha
+            rendered = font.render(current_line, True, (0, 0, 0))
+            window.blit(rendered, (pos_x, line_y_offset))
+            section_width = max(section_width, rendered.get_width())
+            section_height = rendered.get_height() if section_height == 0 else section_height - news_line_spacing
+            line_y_offset += news_line_spacing
+            
+        # Cria uma única caixa para todo o valor
+        sections.append(pygame.Rect(pos_x, pos_y, section_width, section_height))
+    return sections
+
+
+def main():
+    # Inicializa o pygame
     pygame.init()
 
-    # Configurações da janela
-    largura, altura = LARGURA, ALTURA
-    titulo = TITULO
-    cor_fundo = (0, 0, 0)  # Preto
-
     # Criação da janela
-    tela = pygame.display.set_mode((largura, altura))
-    pygame.display.set_caption(titulo)
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption(WINDOW_TITLE)
 
     # Loop principal do jogo
-    rodando = True
+    running = True
 
-    CENARIO = pygame.image.load("src/sprites/cenario.png").convert()
-    MESA = pygame.image.load("src/sprites/mesa.png") .convert()
-    tela.blit(CENARIO, (0, 0))
-    tela.blit(MESA, (0, 340))   
+    background = pygame.image.load(r'sprites\background.png').convert()
+    table = pygame.image.load(r'sprites\table.png').convert()
+    window.blit(background, (0, 0))
+    window.blit(table, (0, 340))
 
-    DADOS = {"data": "18/09/2007", "titulo":"Bressan Mata 7 pessoas em Jundiaí", "autor": "Bressan Hater", "paragrafo1": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi impedit voluptatibus saepe ad eum. Mollitia temporibus voluptatibus magni quo fugit, delectus velit libero praesentium, eligendi repellendus nesciunt tempora sequi. Nam!", "paragrafo2": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi impedit voluptatibus saepe ad eum. Mollitia temporibus voluptatibus magni quo fugit, delectus velit libero praesentium, eligendi repellendus nesciunt tempora sequi. Nam!", "bibliografia": "Minoru Mineta"}
+    data = {
+        'date': '18/09/2007',
+        'title': 'Bressan Mata 7 pessoas em Jundiaí',
+        'author': 'Bressan Hater',
+        'paragrafo1': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi impedit voluptatibus saepe ad eum. Mollitia temporibus voluptatibus magni quo fugit, delectus velit libero praesentium, eligendi repellendus nesciunt tempora sequi. Nam!',
+        'paragrafo2': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi impedit voluptatibus saepe ad eum. Mollitia temporibus voluptatibus magni quo fugit, delectus velit libero praesentium, eligendi repellendus nesciunt tempora sequi. Nam!',
+        'bibliography': 'Minoru Mineta'
+    }
 
-    while rodando:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                rodando = False
-        folha = pygame.image.load("src/sprites/folha.png").convert_alpha()
-        
-        tela.blit(folha, ((largura/2)-folha.size[0]/2, 100))
-        caixas = displayData(DADOS, folha, tela)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        # Verifica cliques nas caixas
-        if pygame.mouse.get_pressed()[0]:  # Verifica se o botão esquerdo do mouse foi pressionado
+        news = pygame.image.load(r'sprites\window.png').convert_alpha()
+
+        window.blit(news, ((WINDOW_WIDTH / 2) - news.size[0] / 2, 100))
+        news_sections = display_data(data, news, window)
+
+        # Verifica cliques nas news_content
+        if pygame.mouse.get_pressed()[0]: # Verifica se o botão esquerdo do mouse foi pressionado
             pos_mouse = pygame.mouse.get_pos()
-            for i, caixa in enumerate(caixas):
-                if caixa.collidepoint(pos_mouse):
-                    print(f"Clicou na caixa: {list(DADOS.keys())[i]}")
-        
+            for i, section in enumerate(news_sections):
+                if section.collidepoint(pos_mouse):
+                    print(f'Clicou na caixa: {list(data.keys())[i]}')
+
         # Atualiza a tela
         pygame.display.flip()
-
 
     # Encerra o pygame
     pygame.quit()
 
-def displayData(data, folha, tela):
-    largura = tela.get_width()
-    fonte = pygame.font.Font(None, 12)
-    y_offset = 120 
-    espaco = 15
-    caixas = []
-    for chave, valor in data.items():
-        texto = f"{chave}: {valor}"
-        palavras = texto.split()
-        linha_atual = ""
-        pos_x = (largura / 2) - folha.get_width() / 2 + 10
-        largura_maxima = folha.get_width() - 20  # Margem dentro da folha
-        caixa_inicio_y = y_offset  # Guarda a posição inicial da caixa
-        largura_caixa = 0  # Largura máxima da caixa
-        altura_caixa = 0  # Altura acumulada da caixa
-
-        for palavra in palavras:
-            if fonte.size(linha_atual + palavra + " ")[0] > largura_maxima:
-                renderizado = fonte.render(linha_atual, True, (0, 0, 0)) 
-                tela.blit(renderizado, (pos_x, y_offset))
-                largura_caixa = max(largura_caixa, renderizado.get_width())
-                altura_caixa += renderizado.get_height()
-                y_offset += espaco
-                altura_caixa += espaco
-                linha_atual = palavra + " "
-                
-            else:
-                if chave == "data":
-                    pos_x = largura/2 + 100
-                linha_atual += palavra + " "
-
-        if linha_atual:  # Renderiza a última linha
-            renderizado = fonte.render(linha_atual, True, (0, 0, 0))
-            tela.blit(renderizado, (pos_x, y_offset))
-            largura_caixa = max(largura_caixa, renderizado.get_width())
-            altura_caixa = renderizado.get_height() if altura_caixa == 0 else altura_caixa - espaco
-            y_offset += espaco
-            
-        # Cria uma única caixa para todo o valor
-        caixas.append(pygame.Rect(pos_x, caixa_inicio_y, largura_caixa, altura_caixa))
-    return caixas    
-
-__main__()
+if __name__ == '__main__':
+    main()
