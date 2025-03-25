de85d85e899b9627ac506336682abe40f859c4be
Merlottera <sasaquerie@gmail.com>
2025-03-25 18:21:42 -0300

    Código Melhorado e atualizado em inglês

--- game/constants.py
+++ game/constants.py
@@ -1,56 +1,26 @@
 import pygame
 
-# Configurações da janela
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 640
 WINDOW_TITLE = 'News Please'
 
-# Cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (100, 100, 100)
 HIGHLIGHT_COLOR = (200, 100, 100)
 
-# Caminhos dos assets
 ASSETS_PATH = {
     'background': 'assets/sprites/background.png',
-    'table': 'assets/sprites/table.png',
-    'news_window': 'assets/sprites/window.png'
+    'news_window': 'assets/sprites/window.png',
+    'menu_teste': 'assets/sprites/menu_background_teste.png'
 }
 
-
-class _FontManager:
+class FontManager:
     def __init__(self):
-        self._small = None
-        self._medium = None
-        self._large = None
+        font_path = 'assets/fonts/MinecraftStandard.otf'
+        self.small = pygame.font.Font(font_path, 12)
+        self.medium = pygame.font.Font(font_path, 24)
+        self.large = pygame.font.Font(font_path, 36)
 
-    @property
-    def SMALL(self):
-        if self._small is None:
-            self._initialize()
-        return self._small
+pygame.init()
 
-    @property
-    def MEDIUM(self):
-        if self._medium is None:
-            self._initialize()
-        return self._medium
-
-    @property
-    def LARGE(self):
-        if self._large is None:
-            self._initialize()
-        return self._large
-
-    def _initialize(self):
-        try:
-            self._small = pygame.font.Font(None, 12)
-            self._medium = pygame.font.Font(None, 24)
-            self._large = pygame.font.Font(None, 36)
-        except:
-            self._small = pygame.font.SysFont('arial', 12)
-            self._medium = pygame.font.SysFont('arial', 24)
-            self._large = pygame.font.SysFont('arial', 36)
-
-
-FONTS = _FontManager()
+FONTS = FontManager()
